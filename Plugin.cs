9843c8a57f9429e3707e5f44d49424a49058abfd
Michael Rooplall <MichaelRooplall@gmail.com>
2024-09-18 15:40:13 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ATTUnlockedSecondFloor;
+
+[BepInPlugin("com.michaelrooplall.mods.attunlockedsecondfloor", "Unlocked Second Floor", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    private void OnDestroy() {
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        if (scene.name == "Playtest") {
+            Invoke("build", 2f);
+        }
+    }
+
+    private void build() {
+        try {
+            this.unlockSecondFloor();
+        } catch (System.Exception e) {
+            Logger.LogError(e);
+        }
+
+    }
+
+    private void unlockSecondFloor() {
+
+        GameObject stairs_r = GameObject.Find("Tavern/Interactive/Zones/Opening Soon Stairs R");
+        GameObject stairs_l = GameObject.Find("Tavern/Interactive/Zones/Opening Soon Stairs L");
+        GameObject walls = GameObject.Find("Tavern/Interactive/Zones/Invisible Walls 2 Floor");
+        GameObject invisibleDivider = GameObject.Find("Tavern/Interactive/Zones/Opening Soon Main/Top Wall");
+        
+        stairs_r.SetActive(false);
+        stairs_l.SetActive(false);
+        walls.SetActive(false);
+        invisibleDivider.SetActive(false);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Ale and Tavern Tale - Unlocked Second Floor
+
+Simple mod that unlocks the second floor.
+
+Removes the two rope barriers on the stairs and other invisible walls upstairs. You must unlock the level eight area to access staircases (or jump over the wall).
+
+Works in Multiplayer. Any client that does not have the mod will not be able to go upstairs. Only use this in servers where the host allows it.
+
+Requires BepInEx.
+
+## NOTE:
+The developers definitely intend to add content to the upstairs area. Build up there at your own risk.
+
+# Manual Installation
+This mod requires BepInEx.
+Place the 'ATTUnlockedSecondFloor' folder into the BepInEx/plugins folder
